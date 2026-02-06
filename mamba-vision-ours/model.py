26491412576164a961e6838fad3c5dc72080ef85
Andrii Matsevytyi <macevitiya@i.ua>
2026-02-06 18:20:48 +0200

    updated model to match all features, ran in the issue that it returnes Bx640 instead of NVIDIA's claimed Bx4x640 size (with 4 scales of features)

--- mamba-vision-ours/model.py
+++ mamba-vision-ours/model.py
@@ -1,24 +1,21 @@
 """Initial model: YOLOv11 Detection Head with Mamba-Vision Backbone."""
 
 import torch
+import argparse
 import torch.nn as nn
-from pathlib import Path
 from typing import List
-import sys
+
+import numpy as np
 
 from yolov11detection import YOLOv11Head
 from yolov11detection import YOLONeck
 
-
-# submodule import playaround
-_current_dir = Path(__file__).resolve().parent
-_reengine_dir = _current_dir.parent / "MambaVisionReengineering"
-if str(_reengine_dir) not in sys.path:
-    sys.path.insert(0, str(_reengine_dir))
+from torchinfo import summary
 
 try:
     # official approach
-    from MambaVisionReengineering.mambavision import create_model 
+    from mambavision import create_model 
+    torch.serialization.add_safe_globals([argparse.Namespace])
 except ImportError:
     raise ImportError("Check if 'mambavision' package/folder is in MambaVisionReengineering")
 
@@ -27,10 +24,11 @@ class MambaVisionOurs(nn.Module):
     
     def __init__(
         self, 
+        device="cuda",
         model_type="mamba_vision_T", 
         num_output_classes=80, 
         pretrained=True,
-        checkpoint_path=''
+        checkpoint_path="mambavision_tiny_1k.pth.tar"
     ):
         """
         Args:
@@ -41,13 +39,28 @@ class MambaVisionOurs(nn.Module):
         super().__init__()
         self.backbone = create_model(
             model_type, 
-            pretrained=pretrained, 
+            pretrained=True, 
             num_classes=0,
-            checkpoint_path=checkpoint_path
+            #checkpoint_path=checkpoint_path
         )
+
+        self.device=device
         
         # MambaVision-T Stage dims: [80, 160, 320, 640]
         self.backbone_dims = [80, 160, 320, 640]
+
+        if pretrained:
+
+            checkpoint = torch.load(checkpoint_path, map_location=self.device)
+            state_dict = checkpoint.get('model', checkpoint)
+
+            # remove head weights
+            backbone_state_dict = {k: v for k, v in state_dict.items() if not k.startswith('head.')}
+
+            missing, unexpected = self.backbone.load_state_dict(backbone_state_dict, strict=False)
+            print("Missing keys:", missing)
+            print("Unexpected keys:", unexpected)
+
         
         # Detection Neck (FPN/PAN style)
         # Using stages 2, 3, 4 (160, 320, 640 channels) for multi-scale detection
@@ -68,11 +81,12 @@ class MambaVisionOurs(nn.Module):
         """
         # in paper mamba vision returns (avg_pool_out, list_of_stage_features)
         # when num_classes=0 or when used as a feature extractor
-        _, features = self.backbone(x)
+        stage_features = self.backbone.forward_features(x)
+        print(np.shape(stage_features))
         
         # features[0]=Stage1, features[1]=Stage2, features[2]=Stage3, features[3]=Stage4
         # pass the last N stages to the YOLO neck
-        neck_out = self.neck(features[1:]) 
+        neck_out = self.neck(stage_features[1:4]) 
         
         head_out = self.head(neck_out)
         
@@ -115,22 +129,29 @@ def check_shapes(model: MambaVisionOurs, input_tensor: torch.Tensor):
 if __name__ == "__main__":
 
     print("Creating MambaYOLO model...")
+
+    # pretrained=False to check shapes or forward pass or head/neck integration
+    # pretrained=True to run actual code, requires GPU
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
     model = MambaVisionOurs(
         model_type="mamba_vision_T",
+        device=device,
         num_output_classes=80,
-        pretrained=True
-    )
-    
-    check_shapes(model)
+        pretrained=True,
+    ).to(device)
+
     
     print("\nModel Summary:")
-    summary = model.summary()
-    for key, value in summary.items():
-        print(f"  {key}: {value}")
+    summary(model.backbone, input_size=(20, 3, 224, 224))
     
     # Test forward pass
     print("\nTesting forward pass...")
-    x = torch.randn(2, 3, 224, 224)
+    x = torch.randn(20, 3, 224, 224).to(device)
+
+    #check_shapes(model, x)
     
     with torch.no_grad():
         outputs = model(x)
